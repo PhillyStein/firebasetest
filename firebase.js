a08ec79eb105fedf5cec3f8a74683d877436fbd7
Philly Stein <37030668+PhillyStein@users.noreply.github.com>
2025-03-08 15:58:09 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -17,7 +17,7 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 const auth = getAuth();
-firebase.auth.Auth.Persistence.LOCAL;
+auth.Auth.Persistence.LOCAL;
 
 async function getBirds(db) {
     console.log("1")
